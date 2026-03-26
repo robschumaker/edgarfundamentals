9a169e7ab3990d3966949f48b3f0293b30105c49
Robert Schumaker <rob.schumaker@gmail.com>
2026-03-26 11:02:53 -0500

    Made the .onAttach code a bit more visually interesting

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 .onAttach <- function(libname, pkgname) {
-  cli::cli_alert_info("edgarfundamentals v{utils::packageVersion('edgarfundamentals')}")
-  cli::cli_alert_warning("Please set your User-Agent before making API calls:")
-  cli::cli_code("options(edgarfundamentals.user_agent = \"Your Name your@email.com\")")
+  cli::cli_rule(
+    left  = "{.strong Attaching edgarfundamentals}",
+    right = paste0("edgarfundamentals ", utils::packageVersion("edgarfundamentals"))
+  )
+  cli::cli_text("{cli::col_red('Set your User-Agent before making API calls:')}")
+  cli::cli_code(" options(edgarfundamentals.user_agent = \"Your Name your@email.com\")")
 }
