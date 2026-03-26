f16cfbbd639affabc4556043c89f60ff0580b143
Robert Schumaker <rob.schumaker@gmail.com>
2026-03-26 11:52:43 -0500

    Fixing Claude making sh*t up

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 .onAttach <- function(libname, pkgname) {
-  msg <- cli:cli_format({
-    cli::cli_rule(
-      left  = "{.strong Attaching edgarfundamentals}",
-      right = paste0("edgarfundamentals ", utils::packageVersion("edgarfundamentals"))
-    )
-    cli::cli_text("{cli::col_blue('Set your User-Agent before making API calls:')}")
-    cli::cli_text(paste0("\u00a0\u00a0options(edgarfundamentals.user_agent = ", cli::col_blue('"Your Name your@email.com"'), ")"))
-    cli::cli_rule()
-  })
-  packageStartupMessage(msg)
+  cli::cli_rule(
+    left  = "{.strong Attaching edgarfundamentals}",
+    right = paste0("edgarfundamentals ", utils::packageVersion("edgarfundamentals")),
+    class = "packageStartupMessage"
+  )
+  cli::cli_text("{cli::col_blue('Set your User-Agent before making API calls:')}",
+                class = "packageStartupMessage")
+  cli::cli_text(paste0("\u00a0\u00a0options(edgarfundamentals.user_agent = ", cli::col_blue('"Your Name your@email.com"'), ")"),
+                class = "packageStartupMessage")
+  cli::cli_rule(class = "packageStartupMessage")
 }
