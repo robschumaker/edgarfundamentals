6bb65e96c089a345823250790e356f8b4c837705
Robert Schumaker <rob.schumaker@gmail.com>
2026-03-26 11:49:47 -0500

    Fix SuppressStartupMessages

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,12 @@
 .onAttach <- function(libname, pkgname) {
-  cli::cli_rule(
-    left  = "{.strong Attaching edgarfundamentals}",
-    right = paste0("edgarfundamentals ", utils::packageVersion("edgarfundamentals"))
-  )
-  cli::cli_text("{cli::col_blue('Set your User-Agent before making API calls:')}")
-  cli::cli_text(paste0("\u00a0\u00a0options(edgarfundamentals.user_agent = ", cli::col_blue('"Your Name your@email.com"'), ")"))
-  cli::cli_rule()
+  msg <- cli:cli_format({
+    cli::cli_rule(
+      left  = "{.strong Attaching edgarfundamentals}",
+      right = paste0("edgarfundamentals ", utils::packageVersion("edgarfundamentals"))
+    )
+    cli::cli_text("{cli::col_blue('Set your User-Agent before making API calls:')}")
+    cli::cli_text(paste0("\u00a0\u00a0options(edgarfundamentals.user_agent = ", cli::col_blue('"Your Name your@email.com"'), ")"))
+    cli::cli_rule()
+  })
+  packageStartupMessage(msg)
 }
